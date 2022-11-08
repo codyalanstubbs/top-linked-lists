f638fcc198acf642f56ba0a0c2d001781de67260
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-11-08 14:39:18 -0500

    Simplify Node factory and add recursive append function to LinkedList

--- linked-lists.js
+++ linked-lists.js
@@ -1,11 +1,24 @@
 const Node = (nodeValue = null, nextNodeLink = null) => {
     let value = nodeValue;
     let nextNode = nextNodeLink;
-
-    return {
-        setValue(newValue) {value = newValue},
-        setNextNode(newNextNodeLink) {nextNode = newNextNodeLink},
-        getValue() {return value},
-        getNextNode() {return nextNode}
-    }
+    return {value, nextNode};
 };
+
+const LinkedList = () => {
+    const head = Node();
+    const list = {head};
+
+    const getList = () => {return list};
+
+    const append = (value, prevNode = list.head) => {
+        if (prevNode.nextNode !== null) {
+            append(value, prevNode.nextNode);
+        } else {
+            prevNode.nextNode = Node(value);
+        }
+        return list;
+    };
+
+    return {getList, append};
+}
+// const newList = LinkedList();
